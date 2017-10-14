db429b517fb5c947f2f4071186dc566888a996ee
JessieM15 <mansonjessie15@gmail.com>
2017-10-14 13:45:43 -0700

    Update Decrypt.c

--- Decrypt.c
+++ Decrypt.c
@@ -1,3 +1,32 @@
+// Jessie Manson
+// CISS 247, Fall 2017
+// Oct 20,20117
+// Assignment 1
+
+//extracts two 28-bit keys from 4bytes
 void get_keys (char *twokeys, unsigned *key1, unsigned *key2){
 
 }
+
+//returns the integer value of a specified subsequence of 32 bitswidthis
+//the number of bits to be extractedindex * widthis the index of the 
+//rightmost bit to extract
+unsigned int get_n_bits (unsigned bits, int width, int index){
+
+}
+
+//given a sequence of 7 bits, rotate 3 bits to the left
+unsigned int rotate_left3(unsigned bits){
+  
+}
+
+//shuffle the 7 nibbles according to the pattern
+void shuffle_nibbles (unsigned *bits){
+
+}
+
+//decode a block of 28 bits
+void decode_28bits (unsigned cipher,char *plain,unsigned key1,unsigned key2){
+
+}
+
