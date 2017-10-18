e17c1d9767d642bdb5e8a469470aca70b3678c71
JessieM15 <mansonjessie15@gmail.com>
2017-10-17 19:10:33 -0700

    Update Decrypt.c

--- Decrypt.c
+++ Decrypt.c
@@ -8,36 +8,6 @@
 
 #define BYTE    8
 
-void getBits(char *cipher){
-  int len = 0;
-  len = strlen(cipher);
-  char c1 [28];
-  //fill key1 with letters
-  for(int i = 0; i < 28; i++){
-    c1[i] = cipher[i];
-  }
-
-  printf("%s\n", c1);
-  //convert k1 to binary
-  for (int i = 0; i < strlen(c1); i++) {
-    //print ascii
-    int count = (int) c1[i];
-    int j = len-2;
-    while(count != 0){
-      if(count%2 == 0){
-        c1[j]=0;
-      }else {
-        c1[j]=1;
-      }
-      count = count/2;
-      j--;
-    }
-    for(int i = 0; i < 27; i++){
-      printf("%d ", c1[i] );
-    }
-printf("\n");
-  }
-}
 //extracts two 28-bit keys from 4bytes
 void get_keys (char *twokeys, unsigned *key1, unsigned *key2){
   int len, len1 = 0;
@@ -66,6 +36,9 @@ void get_keys (char *twokeys, unsigned *key1, unsigned *key2){
       count = count/2;
       j--;
     }
+    // for(int i = 0; i < BYTE-1; i++){
+    //   printf("%d ", key1[i] );
+    // }
     for(int i = 0; i < BYTE-1; i++){
       printf("%d ", key1[i] );
     }
@@ -78,7 +51,7 @@ printf("\n");
     printf("%c ", k2[i]);
   }
   printf("\n");
-  //printf("%d\n",strlen(k2));
+
   //convert k2 to binary
   for (int i = len1; i < len; i++) {
     //print ascii
@@ -86,30 +59,21 @@ printf("\n");
 
     int count = (int) k2[i];
     int j = BYTE-2;
-    //getBits(count, j, );
-    //printf("%d\n",j );
+
     while(count != 0){
-      //printf("%d\n",count%2 );
       if(count%2 == 0){
-        //printf("index %d \n",j );
-        //printf("bit %d \n",count%2 );
-        //printf("index %d \n",j );
         key2[j]=0;
       }else {
-        //printf("index %d \n",j );
         key2[j]=1;
-        //printf("bit: %d \n",key1[j] );
-        //printf("index :%d \n ",j );
       }
       count = count/2;
-       //printf("%d\n",count );
-      //printf("%d ", key1[j]);
       j--;
     }
+
     for(int i = 0; i < BYTE-1; i++){
       printf("%d ", key2[i] );
     }
-printf("\n");
+    printf("\n");
   }
 }
 
@@ -123,9 +87,12 @@ printf("\n");
 // }
 //
 //given a sequence of 7 bits, rotate 3 bits to the left
-// unsigned int rotate_left3(unsigned bits){
+// void rotate_left3(unsigned *bits){
 //     bits = bits << 3;
-//     return bits;
+//     //for(int i = 0; i < 28; i++){
+//       printf("%d ", bits);
+//     //}
+//
 // }
 //
 // //shuffle the 7 nibbles according to the pattern
@@ -138,9 +105,9 @@ printf("\n");
 //
 // }
 
- void exor(int *key1, int *cipher){
+ void exor1(int *key1, int *cipher){
   //int len = strlen(key1);
-  printf("\n xor:");
+  printf("\nxor1: ");
   int bits[28];
   for(int i=0; i<= 28; i++){
     bits[i] = key1[i] ^ cipher[i];
@@ -148,6 +115,20 @@ printf("\n");
   for(int i = 0; i < 28; i++){
     printf("%d ", bits[i] );
   }
+printf("\n");
+}
+
+
+void exor2(int *key2, int *cipher){
+ //int len = strlen(key1);
+ printf("\nxor2: ");
+ int bits[28];
+ for(int i=0; i<= 28; i++){
+   bits[i] = key2[i] ^ cipher[i];
+ }
+ for(int i = 0; i < 28; i++){
+   printf("%d ", bits[i] );
+ }
 printf("\n");
 }
 
@@ -160,7 +141,7 @@ int main(int argc, char *argv[]){
   // get_n_bits
   // shuffle_nibbles();
   // exor(); //with key 1
-  //bits = rotate_left3(bits);
+  // bits = rotate_left3(bits);
   // decode_28bits();
 
   // char line[8];
@@ -179,6 +160,7 @@ int main(int argc, char *argv[]){
   unsigned key2 [BYTE];
   get_keys(argv[1],key1,key2);
   //unsigned bits [strlen(cipher)];
-  exor(key1, cipher);
+  exor1(key1, cipher);
+  exor2(key2, cipher);
 
 }
