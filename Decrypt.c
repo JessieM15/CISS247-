cbf2d1ac3b726006fdd6f7f7ccfaf3f76b367b5b
JessieM15 <mansonjessie15@gmail.com>
2017-10-18 18:13:34 -0700

    Update Decrypt.c

--- Decrypt.c
+++ Decrypt.c
@@ -10,71 +10,45 @@
 
 //extracts two 28-bit keys from 4bytes
 void get_keys (char *twokeys, unsigned *key1, unsigned *key2){
-  int len, len1 = 0;
+
+  int len, len1,x = 0;
   printf("%s\n", twokeys);
   len = strlen(twokeys);
   len1 = len / 2;
+  //printf("%d\n", len1);
   char k1 [len1];
   char k2 [len1];
-  //fill key1 with letters
+  //fill temp array with letters
   for(int i = 0; i < len1; i++){
     k1[i] = twokeys[i];
+    printf("%c", k1[i]);
   }
-
-  printf("%s\n", k1);
-  //convert k1 to binary
-  for (int i = 0; i < strlen(k1); i++) {
-    //print ascii
-    int count = (int) k1[i];
-    int j = BYTE-2;
-    while(count != 0){
-      if(count%2 == 0){
-        key1[j]=0;
-      }else {
-        key1[j]=1;
-      }
-      count = count/2;
-      j--;
-    }
-    // for(int i = 0; i < BYTE-1; i++){
-    //   printf("%d ", key1[i] );
-    // }
-    for(int i = 0; i < BYTE-1; i++){
-      printf("%d ", key1[i] );
-    }
-printf("\n");
-  }
+  //take temp array and fill with key1s
+  key1= (k1[0]<<21)+(k1[1]<<14)+(k1[2]<<7)+(k1[3]);
+  // x = 21;
+  // for (int i = 0; i < len1-1; i++) {
+  //   key1 += (k1[i]<<x);
+  //   x-=7;
+  //   printf("\n%d",key1);
+  //
+  // }
+   printf("\n%d\n",key1);
 
   //fill key2 with letters
   for(int i = len1; i < len; i++){
     k2[i] = twokeys[i];
-    printf("%c ", k2[i]);
+    printf("%c", k2[i]);
   }
-  printf("\n");
+  key2 = (k2[0]<<21)+(k2[1]<<14)+(k2[2]<<7)+(k2[3]);
+  // x = 21;
+  // for (int i = 0; i < len1-1; i++) {
+  //   key2 += k1[i]<<x;
+  //
+  //   x-=7;
+  // }
+   printf("\n%d\n",key2);
+  //
 
-  //convert k2 to binary
-  for (int i = len1; i < len; i++) {
-    //print ascii
-    //printf("%d\n", (int) k2[i]);
-
-    int count = (int) k2[i];
-    int j = BYTE-2;
-
-    while(count != 0){
-      if(count%2 == 0){
-        key2[j]=0;
-      }else {
-        key2[j]=1;
-      }
-      count = count/2;
-      j--;
-    }
-
-    for(int i = 0; i < BYTE-1; i++){
-      printf("%d ", key2[i] );
-    }
-    printf("\n");
-  }
 }
 
 //
@@ -117,20 +91,20 @@ printf("\n");
   }
 printf("\n");
 }
-
-
-void exor2(int *key2, int *cipher){
- //int len = strlen(key1);
- printf("\nxor2: ");
- int bits[28];
- for(int i=0; i<= 28; i++){
-   bits[i] = key2[i] ^ cipher[i];
- }
- for(int i = 0; i < 28; i++){
-   printf("%d ", bits[i] );
- }
-printf("\n");
-}
+//
+//
+// void exor2(int *key2, int *cipher){
+//  //int len = strlen(key1);
+//  printf("\nxor2: ");
+//  int bits[28];
+//  for(int i=0; i<= 28; i++){
+//    bits[i] = key2[i] ^ cipher[i];
+//  }
+//  for(int i = 0; i < 28; i++){
+//    printf("%d ", bits[i] );
+//  }
+// printf("\n");
+// }
 
 //main
 int main(int argc, char *argv[]){
@@ -154,13 +128,13 @@ int main(int argc, char *argv[]){
   //   x = sscanf(line,"%x", &bits);
   //   printf("%d\n", x );
   // }
-  int cipher[29] = {1,1,0,1 ,1,0,1,1, 1,1,0,0 ,0,0,1,0, 0,0,1,0, 0,0,1,1, 0,0,1,1};
+
   //getBits(cipher);
   unsigned key1 [BYTE];
   unsigned key2 [BYTE];
   get_keys(argv[1],key1,key2);
   //unsigned bits [strlen(cipher)];
   exor1(key1, cipher);
-  exor2(key2, cipher);
+  // exor2(key2, cipher);
 
 }
