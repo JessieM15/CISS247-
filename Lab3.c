72ec4100f076d078020e8dd6f0eea168daa6901e
JessieM15 <mansonjessie15@gmail.com>
2017-10-23 11:50:05 -0700

    func 2 works

--- Lab3.c
+++ Lab3.c
@@ -31,20 +31,45 @@ void func1(int a, int b){
 // of two integers, using onlythe~ and &operators.
 // int func2(int x, int y);
 // For example, func2(4, 5)equals1.  (0100 ^ 0101 = 0001).
+//Not 'a' then & them together
 
 void func2(int a, int b){
+
   int x = 0;
 
-  a = a >> 2;
+  a = ~a;
 
   x = a & b;
   printf("%d\n",x );
 
+}
+/*Write a function to return a bit
+-
+mask with all 1's between lowbit and highbit, inclusive.
+Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31.
+ If lowbit > highbit, then mask should be all 0's.
+int func5(int highbit, int lowbit);
+For example,
+func5(5, 3);
+should return 56 (0x38 = 00111000
+*/
+// shift the bits so only ones between a and b
+void func5(int a, int b){
+    int x = 128;
+    int y = 128;
+    int ans;
+
+    x = x >> a;
+    y = y << b;
+
+    ans = x | y;
+
+    printf("%d\n", x);
 }
 
 
 int main(int count, char *word[]) {
     func1(4,5);
     func2(4,5);
-
+    func5(5,3);
 }
