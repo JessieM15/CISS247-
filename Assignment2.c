7f063c2a34bbc993225e4cd5645f2396ce540fbb
JessieM15 <mansonjessie15@gmail.com>
2017-11-13 10:02:35 -0800

    Update Assignment2.c

--- Assignment2.c
+++ Assignment2.c
@@ -1,20 +1,27 @@
 #define MAX_WORD_LEN 40
 #define MAX_WORDS 110000
-char wordlist[MAX_WORDS][MAX_WORD_LEN];
 
+//declare array 110k long each with 40 characters
+char wordlist[MAX_WORDS][MAX_WORD_LEN];
+//Then you can access the ith word in the list as wordlist[i].
 void fillWordArray(fullArray []){
   for (int i = 0; i < sizeof(fullArray); i++) {
    if(fullArray [i] == '\n'){
      
-  }
+   }
 
+  }
 }
 
 int main(int argc, char *argv[]) {
 
+  if(sizeof(argv) < 2){
+    printf("Format of Command is as followed : ");
+    return 0;
+  }else{
+
+
+  }
   FILE *input = fopen(argv[1], “r”);
-  //. . .
   fgets(wordlist[i], MAX_WORD_LEN, input);
-  char word[MAX_WORD_LEN];
-  fgets(word, MAX_WORD_LEN, stdin);
 }
