bf493c6cd5bcd244a786197666f70b6ac8020f4e
JessieM15 <mansonjessie15@gmail.com>
2017-11-13 19:39:30 -0800

    Update A2.c

--- A2.c
+++ A2.c
@@ -1,12 +1,5 @@
 int main(int argc, char *argv[]) {
-  // need to take input and make it all lower cases
-  // so can later be compared
-
-  //  if(strlen(argv) < 2){
-  //    printf("Format of Command is as followed : ./Assignment2 wordlist.txt ");
-  //    return 0;
-  //  }
-
+  
     char wordlist[MAX_WORDS][MAX_WORD_LEN];
     
     FILE *input = fopen(argv[1], "r");
@@ -16,11 +9,7 @@ int main(int argc, char *argv[]) {
       fgets(wordlist[i], MAX_WORD_LEN, input);
 
     }
-
-//  }
-  //get word from user
   char word[MAX_WORD_LEN];
   fgets(word, MAX_WORD_LEN, stdin);
 
-  //fillWordArray( wordlist);
 }
