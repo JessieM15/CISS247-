1ec4f05949c9354682166e2607feba3d7cad4735
JessieM15 <mansonjessie15@gmail.com>
2017-11-13 19:21:11 -0800

    Update Assignment2.c

--- Assignment2.c
+++ Assignment2.c
@@ -58,16 +58,23 @@ int main(int argc, char *argv[]) {
   // need to take input and make it all lower cases
   // so can later be compared
 
-  if(sizeof(argv) <= 2){
-    printf("Format of Command is as followed : ");
-    return 0;
-  }else{
+   if(strlen(argv) < 2){
+     printf("Format of Command is as followed : ./Assignment2 wordlist.txt ");
+     return 0;
+   }
+
     char wordlist[MAX_WORDS][MAX_WORD_LEN];
-    FILE *input = fopen(argv[1], “r”);
+    FILE *input = fopen(argv[1], "r");
+    printf("test" );
     for (int i = 0; i < sizeof(input); i++) {
       fgets(wordlist[i], MAX_WORD_LEN, input);
-    }
-  }
 
-  fillWordArray( wordlist);
+    }
+
+//  }
+  //get word from user
+  char word[MAX_WORD_LEN];
+  fgets(word, MAX_WORD_LEN, stdin);
+
+  //fillWordArray( wordlist);
 }
